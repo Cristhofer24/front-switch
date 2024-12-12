9300d54b8fc7e064f9bb87e3640ae9d7c76d6651
karolinagit3007 <karolina.17hidalgo@gmail.com>
2024-12-12 13:21:45 -0500

    Datos de la tabla

--- src/app/shared/components/main/main.component.ts
+++ src/app/shared/components/main/main.component.ts
@@ -1,31 +1,44 @@
-import { Component } from '@angular/core';
-import { FitSwitchAPIService } from '../../../Servicios/fit-switch-api.service';
+import { Component, OnInit } from '@angular/core';
+//import { FitSwitchAPIService } from '../../../Servicios/fit-switch-api.service';
 import { NgFor } from '@angular/common';
+import { IsoService } from '../../../Servicios/ISO/iso.service';
+import { Subscription } from 'rxjs';
 //import { FitSwitch } from '../../../Interface/InterfaceFitSwitch';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgxPaginationModule],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
-  campos: FitSwitch[] = [];
-  constructor(private apiservice:FitSwitchAPIService){}
+export class MainComponent implements OnInit {
+  isoData: any[] = [];
+  currentPage: number = 1; 
+  itemsPerPage: number = 10; 
+  private subscription: Subscription | null = null;
 
+  constructor(private isoService: IsoService) {}
 
   ngOnInit(): void {
-    // Llamar al servicio para obtener los usuarios
-    this.apiservice.listarCampos().subscribe(
-      (data) => {
-        this.campos = data; // Asigna los usuarios a la variable
-        console.log('Datos obtenidos:', this.campos); // Verifica si los datos llegan
-      },
-      (error) => {
-        console.error('Error al obtener los datos:', error); // Muestra errores si ocurren
-      }
-    );
+    this.loadIsoData();
   }
+
+  loadIsoData(): void {
+    this.subscription = this.isoService.getIsoData().subscribe({
+      next: (data) => {
+        this.isoData = data;
+      },
+      error: (err) => {
+        console.error('Error fetching ISO data:', err);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
 
 }
