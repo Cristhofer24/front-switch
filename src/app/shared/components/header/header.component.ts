bed6cf4b24aa7fcebfdab2f1207d920bdbf2e648
cristhofer24 <you@example.com>
2024-12-27 16:29:17 -0500

    Cambios para implmentar la lectura del XML

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { UserComponent } from "./components/user/user.component";
+import { TimeComponent } from "./components/time/time.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [UserComponent],
+  imports: [UserComponent, TimeComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
